789dccfe0a4f6cf2a6f19d19c776e34d08ce904f
Yogesh Panchal <yogeshpanchal28082001@gmail.com>
2023-08-05 08:20:59 +0530

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+export const Player = ({fn, song})=>{
+    console.log('Song Object Rec ', song);
+    return (
+    <div>
+        
+        <button onClick={()=>{
+            fn(false, null);
+        }} className = 'btn btn-success'>Back to Songs</button>
+        <p>
+        <img src = {song.artworkUrl100}/>
+        <br/>
+        {song?.artistName} {song?.trackName}
+        </p>
+        <br/>
+        <br/>
+    <audio controls>
+        
+        <source src={song?.previewUrl} type="audio/mp4"/>
+      Your browser does not support the audio element.
+      </audio>
+      </div>)
+}
---
+++ src/components/Songs.jsx
@@ -0,0 +1,10 @@
+import { Song } from "./Song"
+
+export const Songs =({fn,allsongs})=>{
+    console.log('*******All Songs ', allsongs);
+    // map (JS)
+    // allsongs (data) ---> convert --> JSX
+    return (<>
+            {allsongs.map((currentSong,index)=><Song fn = {fn} key={index} song = {currentSong}/>)}
+    </>)
+}
